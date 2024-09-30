97e119f35cfbba0941bf372a596ef3c7b4f5fa83
m1ndos <csgoezkatka@gmail.com>
2024-09-30 13:07:00 +0400

    changed server localhost port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes/routes');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
